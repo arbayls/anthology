573616fab7db8dca697753caa9649adfeac82e25
ellebayless <elle@ellebayles.com>
2017-07-12 09:32:19 -0700

    config diffbot to get access to author data

--- src/App.js
+++ src/App.js
@@ -1,64 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Diffbot from './diffbot.js'
-import axios from '.axios' //use for API call
+import Diffbot from 'diffbot-node-client'
 
 class App extends Component {
-
-  //put functions here
-  var client = new Diffbot("29e49e8b36b2d63c2cf3ed4cf26e584b");
-
-  function getData (url) {
-    client.article.get({
-              url: url
-          }, function onSuccess(response) {
-              // output the title
-              console.log(response);
-              var element = document.getElementById("content");
-              element.innerHTML = response["objects"][0]["title"];
-          }, function onError(response) {
-                switch(response.errorCode) {
-                  case 401:
-                      alert(response.error)
-                      break;
-                  case 404:
-                      alert(response.error)
-                      break;
-                  case 500:
-                      alert(response.error)
-                      break;
-                }
-          });
-  }
-
-  function dataGetter () {
-  var author = ($('.author').val());
-  var firstName = author.split(' ')[0];
-  var lastName = author.split(' ')[1];
-  var twitterHandle = ($('.handle').val());
-  var incMag = ('https://www.inc.com/author/' + firstName + '-' + lastName)
-  var twitter = ('https://twitter.com/' + twitterHandle)
-  var businessInsider = ('http://www.businessinsider.com/author/' + firstName + '-' + lastName)
-  getData(incMag)
-  getData(twitter)
-  getData(businessInsider)
-})
-
-  //use on click method
 
   render() {
     return (
       <div className="App">
       <h2>Author Information Form</h2>
-      <form class="api">  //on submit here
-          <input class="author" type="text" name="Author" value="Author">
-          <input class="handle" type="text" name="Handle" value="Handle">
-          <button class="submit" type="button" name="Submit" >Submit</button> //on click here
+      <form className="api" onSubmit={getData}>
+          <input id="author" type="text" name="Author" placeholder="Author"></input>
+          <input id="handle" type="text" name="Handle" placeholder="Handle"></input>
+          <button id="submit" type="button" name="Submit" onClick={dataGetter}>Submit</button>
       </form>
       </div>
     );
   }
+}
+
+function getData (url) {
+  var client = new Diffbot("29e49e8b36b2d63c2cf3ed4cf26e584b")
+  client.article.get({
+            url: url
+        }, function onSuccess(response) {
+          // console.log('Here is your error! ', err);
+          console.log(JSON.parse(response));
+            // output the title
+            // console.log('Here is your response data! ',response);
+            // var element = document.getElementById("content");
+            // element.innerHTML = response["objects"][0]["title"];
+        }, function onError(response) {
+          console.log(JSON.parse(response));
+        })
+      }
+        // function onError(response) {
+        //       switch(response.errorCode) {
+        //         case 401:
+        //             alert(response.error)
+        //             break;
+        //         case 404:
+        //             alert(response.error)
+        //             break;
+        //         case 500:
+        //             alert(response.error)
+        //             break;
+        //       }
+        // });
+// }
+
+function dataGetter () {
+var author = document.querySelector('#author').value;
+var firstName = author.split(' ')[0];
+var lastName = author.split(' ')[1];
+var twitterHandle = document.querySelector('#handle').value;
+var incMag = ('https://www.inc.com/author/' + firstName + '-' + lastName)
+var twitter = ('https://twitter.com/' + twitterHandle)
+var businessInsider = ('http://www.businessinsider.com/author/' + firstName.toLowerCase() + '-' + lastName.toLowerCase() + "/date/desc")
+getData(incMag)
+getData(twitter)
+getData(businessInsider)
 }
 
 export default App;
