4cb0b83dc72fb8050c6a2bc2ff84a1a0c0e6a0ab
ellebayless <elle@ellebayles.com>
2017-07-12 13:07:51 -0700

    data from inc. mag successfully parsed

--- src/App.js
+++ src/App.js
@@ -18,47 +18,41 @@ class App extends Component {
   }
 }
 
-function getData (url) {
+function getData (url, callback) {
   var client = new Diffbot("29e49e8b36b2d63c2cf3ed4cf26e584b")
   client.article.get({
-            url: url
-        }, function onSuccess(response) {
-          // console.log('Here is your error! ', err);
-          console.log(JSON.parse(response));
-            // output the title
-            // console.log('Here is your response data! ',response);
-            // var element = document.getElementById("content");
-            // element.innerHTML = response["objects"][0]["title"];
-        }, function onError(response) {
-          console.log(JSON.parse(response));
-        })
-      }
-        // function onError(response) {
-        //       switch(response.errorCode) {
-        //         case 401:
-        //             alert(response.error)
-        //             break;
-        //         case 404:
-        //             alert(response.error)
-        //             break;
-        //         case 500:
-        //             alert(response.error)
-        //             break;
-        //       }
-        // });
-// }
+    url: url
+    }, function onSuccess(response) {
+        callback(response)
+    }, function onError(response) {
+        console.log(JSON.parse(response));
+    })
+}
+
 
 function dataGetter () {
 var author = document.querySelector('#author').value;
 var firstName = author.split(' ')[0];
 var lastName = author.split(' ')[1];
 var twitterHandle = document.querySelector('#handle').value;
-var incMag = ('https://www.inc.com/author/' + firstName + '-' + lastName)
+var incMag = ('https://www.inc.com/author/' + firstName.toUpperCase() + '-' + lastName.toUpperCase())
 var twitter = ('https://twitter.com/' + twitterHandle)
 var businessInsider = ('http://www.businessinsider.com/author/' + firstName.toLowerCase() + '-' + lastName.toLowerCase() + "/date/desc")
-getData(incMag)
+getData(incMag, incParser)
 getData(twitter)
 getData(businessInsider)
 }
+
+
+//Parses data received from inc and returns http addresses written by freelancer
+function incParser(response) {
+  var parsedData = JSON.parse(response);
+  var allArticles = parsedData.objects[0].links;
+  var authorArticles = allArticles.filter(function(article) {
+    return article.includes("https://www.inc.com/larry-kim/");
+  })
+  return authorArticles;
+}
+
 
 export default App;
