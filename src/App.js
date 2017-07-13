6571aff54a1a7a7ffdfe396576f95e45f8a4d59e
ellebayless <elle@ellebayles.com>
2017-07-12 20:04:58 -0700

    data from medium successfully parsed

--- src/App.js
+++ src/App.js
@@ -18,40 +18,80 @@ class App extends Component {
   }
 }
 
-function getData (url, callback) {
+function getData (url, callback, author, twitterHandle) {
   var client = new Diffbot("29e49e8b36b2d63c2cf3ed4cf26e584b")
   client.article.get({
     url: url
     }, function onSuccess(response) {
-        callback(response)
+        callback(response, author)
     }, function onError(response) {
         console.log(JSON.parse(response));
     })
 }
 
-
+//formats input from form into searchable http addresses, and fires getData functions for individual api calls.
 function dataGetter () {
-var author = document.querySelector('#author').value;
-var firstName = author.split(' ')[0];
-var lastName = author.split(' ')[1];
-var twitterHandle = document.querySelector('#handle').value;
-var incMag = ('https://www.inc.com/author/' + firstName.toUpperCase() + '-' + lastName.toUpperCase())
-var twitter = ('https://twitter.com/' + twitterHandle)
-var businessInsider = ('http://www.businessinsider.com/author/' + firstName.toLowerCase() + '-' + lastName.toLowerCase() + "/date/desc")
-getData(incMag, incParser)
-getData(twitter)
-getData(businessInsider)
+  var author = document.querySelector('#author').value;
+  var firstName = author.split(' ')[0];
+  var lastName = author.split(' ')[1];
+  var twitterHandle = document.querySelector('#handle').value;
+  var incMag = ('https://www.inc.com/author/' + firstName.toUpperCase() + '-' + lastName.toUpperCase())
+  var twitter = ('https://twitter.com/' + twitterHandle)
+  var businessInsider = ('http://www.businessinsider.com/author/' + firstName.toLowerCase() + '-' + lastName.toLowerCase() + "/date/desc")
+  var medium = ('https://www.medium.com/' + '@' + twitterHandle)
+
+  //object passed into incParser
+  var incObj = {
+    firstName: firstName.toLowerCase(),
+    lastName: lastName.toLowerCase()
+  }
+  getData(incMag, incParser, incObj)
+
+  //object passed into businessInsiderParser
+  var businessInsiderObj = {
+    firstName: firstName.toLowerCase(),
+    lastName: lastName.toLowerCase()
+  }
+  getData(businessInsider, businessInsiderParser, businessInsiderObj)
+
+  var mediumObj = {
+    twitterHandle: twitterHandle
+  }
+  getData(medium, mediumParser, mediumObj)
 }
 
 
 //Parses data received from inc and returns http addresses written by freelancer
-function incParser(response) {
+function incParser(response, author) {
   var parsedData = JSON.parse(response);
-  var allArticles = parsedData.objects[0].links;
-  var authorArticles = allArticles.filter(function(article) {
-    return article.includes("https://www.inc.com/larry-kim/");
+  var allIncArticles = parsedData.objects[0].links;
+  var incAuthorArticles = allIncArticles.filter(function(article) {
+    return article.includes("www.inc.com/" + author.firstName + "-" + author.lastName);
   })
-  return authorArticles;
+  console.log("Here is the inc data ", incAuthorArticles);
+  return incAuthorArticles;
+}
+
+//Parses data received from business insider and returns http addresses written by freelancer
+function businessInsiderParser(response, author) {
+  var parsedData = JSON.parse(response);
+  var allBusinessInsiderArticles = parsedData.objects[0].links;
+  var businessInsiderAuthorArticles = allBusinessInsiderArticles.filter(function(article) {
+    return article.includes("www.businessinsider.com/");
+  })
+  console.log("Here is the business insider data ", businessInsiderAuthorArticles);
+  return businessInsiderAuthorArticles;
+}
+
+function mediumParser(response, author) {
+  var parsedData = JSON.parse(response);
+  console.log("Here is the medium data ", parsedData);
+  // var allMediumArticles = parsedData.objects[0].links;
+  // var mediumAuthorArticles = allMediumArticles.filter(function(article) {
+  //   return article.includes("www.medium.com/");
+  // })
+  // console.log("Here is the medium data ", mediumAuthorArticles);
+  // return mediumAuthorArticles;
 }
 
 
